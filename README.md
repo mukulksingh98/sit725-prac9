e33c7c85b9dd9eeac16d51b5382f4e039aa3c6b2
mukulksingh98 <109273622+mukulksingh98@users.noreply.github.com>
2022-09-25 11:24:22 +0530

    Initial

---
+++ README.md
@@ -0,0 +1,10 @@
+# sit-725-prac9
+
+Docker command to build:
+
+docker build -t <IMAGE_NAME> .
+Commands to run the docker file of this project:
+
+docker pull mukulksingh98/sit725-p9
+docker run -d -p <REPLACE_PORT>:8080 mukulksingh98/sit725-prac9
+Please replace the above. The docker will be running on the value provided in the command above.
---
+++ server.js
@@ -0,0 +1,105 @@
+var express = require("express")
+
+var app = express()
+var cors = require('cors');
+const { Socket } = require("socket.io");
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+let projectsCollection; 
+
+app.use(express.static(__dirname+'/public'))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors())
+
+const MongoClient = require('mongodb').MongoClient
+
+const uri = 'mongodb+srv://admin:admin@cluster0.pohyldt.mongodb.net/?retryWrites=true&w=majority'
+const client = new MongoClient(uri, {useNewUrlParser: true})
+
+const createColllection = (collectionName) => {
+    client.connect((err,db) => {
+        projectsCollection = client.db().collection(collectionName);
+        if(!err) {
+            console.log('DB Connected')
+        }
+        else {
+            console.log("DB Connection Error: ", err);
+            process.exit(1);
+        }
+    })
+}
+
+const insertProjects = (project, callback) => {
+    projectsCollection.insert(project,callback);
+}
+
+const getProjects = (callback) => {
+    projectsCollection.find({}).toArray(callback);
+}
+
+const cardList = [
+    {
+        title: "Kitten 2",
+        image: "images/kitten-2.jpg",
+        link: "About Kitten 2",
+        desciption: "Demo desciption about kitten 2"
+    },
+    {
+        title: "Kitten 3",
+        image: "images/kitten-3.jpg",
+        link: "About Kitten 3",
+        desciption: "Demo desciption about kitten 3"
+    }
+]
+
+app.get('/addNumber/:n1/:n2', (req, res)=>{
+    result = parseInt(req.params.n1) + parseInt(req.params.n2)
+    res.json({statusCode: 200, data: result})
+})
+
+app.post('/api/projects',(req,res) => {
+    console.log("New Project added", req.body)
+    var newProject = req.body;
+    insertProjects(newProject,(err,result) => {
+        if(err) {
+            res.json({statusCode: 400, message: err})
+        }
+        else {
+            res.json({statusCode: 200, message:"Project added", data: result})
+        }
+    })
+})
+
+app.get('/api/projects',(req,res) => {
+    getProjects((err,result) => {
+        if(err) {
+            res.json({statusCode: 400, message: err})
+        }
+        else {
+            res.json({statusCode: 200, message:"Success", data: result})
+        }
+    })
+})
+
+
+io.on('connection', (socket) => {
+    console.log('a user connected', socket.id);
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
+    setInterval(() => {
+      socket.emit('number', new Date().toISOString());
+    }, 1000);
+    setInterval(() => {
+      socket.emit('random_number', parseInt(Math.random() * 10));
+    });
+  });
+
+var port = process.env.port || 3010;
+
+http.listen(port,()=>{
+    console.log("App listening to: "+port)
+    createColllection('Pets')
+})
